7d7290ad84f9271a8a918a53d705aae0c97167ac
bbq-all-stars <8654703+bbq-all-stars@users.noreply.github.com>
2024-08-29 14:50:06 +0900

    add test

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Handson_ShunTabata"
+    Name = "Handson_ShunTabata_hoge"
     TED = "20240829_Handson"
   }
 }
